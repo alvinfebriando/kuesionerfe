5e5ff48ece81daf73a356267d263f9f4d5a88c11
Alvin Febriando <alvin.febriando@outlook.com>
2022-09-25 19:45:01 +0700

    Add more detail to layout components

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,5 +1,44 @@
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+function Copyright() {
+  return (
+    <Typography variant='body2' color='text.secondary'>
+      {'Copyright © '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '90vh',
+      }}
+    >
+      <Box
+        component='footer'
+        sx={{
+          py: 3,
+          px: 2,
+          mt: 'auto',
+          backgroundColor: theme =>
+            theme.palette.mode === 'light'
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800],
+        }}
+      >
+        <Container maxWidth='md'>
+          <Copyright />
+        </Container>
+      </Box>
+    </Box>
+  );
 };
 
 export default Footer;
--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -3,22 +3,18 @@ import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { Container } from '@mui/system';
 
 const Header = () => {
   return (
     <Box mb={2} sx={{ flexGrow: 1 }}>
       <AppBar position='static'>
         <Toolbar>
-          <IconButton
-            size='large'
-            edge='start'
-            color='inherit'
-            aria-label='menu'
-            sx={{ mr: 2 }}
-          ></IconButton>
-          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-            Questionnaire
-          </Typography>
+          <Container maxWidth='md'>
+            <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+              Questionnaire
+            </Typography>
+          </Container>
         </Toolbar>
       </AppBar>
     </Box>
