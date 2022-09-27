ea680a65ca7b56b0628f4fafe59c6ae353f95c08
Alvin Febriando <alvin.febriando@outlook.com>
2022-09-27 11:36:55 +0700

    Remove unused props type

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,14 +1,9 @@
 import Container from '@mui/material/Container';
-import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from './footer';
 import Header from './header';
 
-interface Props {
-  children?: React.ReactNode;
-}
-
-const Layout: React.FC<Props> = props => {
+const Layout = () => {
   return (
     <>
       <Header />
